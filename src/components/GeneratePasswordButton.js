d123d2c17cf82c09b037411796c28f6d311f0ccb
MJ <code.za@proton.me>
2023-04-09 22:34:33 +0200

    Update GeneratePasswordButton.js

--- src/components/GeneratePasswordButton.js
+++ src/components/GeneratePasswordButton.js
@@ -11,7 +11,7 @@ export default function GeneratePasswordButton() {
 
   return (
     <div className="">
-      <button className="text-darkgrey text-m font-normal bg-green w-full h-16 flex items-center justify-center">
+      <button className="text-darkgrey text-m font-normal bg-green w-full h-14 flex items-center justify-center">
         GENERATE
         <div className="ml-5"><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg></div>
       </button>

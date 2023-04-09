7205b732e87cca8aaa7e661f41a9e28ce45a1397
MJ <112978328+MJ-builds@users.noreply.github.com>
2023-04-09 12:07:22 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'theme_bg': '#24232C',
+      'button_green': '#A4FFAF',
+      'slider_bg': '#18171F',
+    }
   },
   plugins: [],
 }

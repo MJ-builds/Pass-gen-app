75aad8be79b8f5ed8ff7eca9ba7345532ab6e960
MJ <code.za@proton.me>
2023-04-09 22:41:27 +0200

    Update GeneratePasswordButton.js

--- src/components/GeneratePasswordButton.js
+++ src/components/GeneratePasswordButton.js
@@ -1,17 +1,18 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { PasswordContext } from "../context/PasswordContext";
 
 export default function GeneratePasswordButton() {
-  const { length, options, generateRandomPassword } =
-    useContext(PasswordContext);
+  const { length, options, generateRandomPassword } = useContext(PasswordContext);
 
-  useEffect(() => {
+  const handleClick = () => {
     generateRandomPassword(length, options);
-  }, []); // Pass an empty dependency array to run this effect only on mount
+  }
 
   return (
     <div className="">
-      <button className="text-darkgrey text-m font-normal bg-green w-full h-14 flex items-center justify-center">
+      <button className="text-darkgrey text-m font-normal bg-green w-full h-14 flex items-center justify-center"
+      onClick={handleClick}
+      >
         GENERATE
         <div className="ml-5"><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg></div>
       </button>

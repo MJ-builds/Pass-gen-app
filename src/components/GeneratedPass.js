3298d0e940f4147f55172da38a117d9d9edaaf8f
MJ <code.za@proton.me>
2023-04-10 22:35:17 +0200

    Added placeholder feature

--- src/components/GeneratedPass.js
+++ src/components/GeneratedPass.js
@@ -4,10 +4,13 @@ import { PasswordContext } from "../context/PasswordContext";
 export default function GeneratedPass() {
   const { password } = useContext(PasswordContext);
 
+  const placeholderStyle = password ? "" : "text-grey italic text-xs opacity-40";
+
   return (
     <div>
-      <div className="p-4 ">
-        <p className="text-2xl font-normal ml-5 break-all">{password}</p>
+      <div className="p-4">
+        <p className={`text-2xl font-normal ml-5 break-all ${placeholderStyle}`}>
+        {password || "To Generate: Tick Options > 0 & Length > 0"}</p>
       </div>
     </div>
   );

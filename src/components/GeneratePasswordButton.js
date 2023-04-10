a939508839af6bacd9de6f5e584641b6af4b9adf
MJ <code.za@proton.me>
2023-04-10 09:38:51 +0200

    Added button click extras

--- src/components/GeneratePasswordButton.js
+++ src/components/GeneratePasswordButton.js
@@ -2,33 +2,53 @@ import React, { useContext, useState } from "react";
 import { PasswordContext } from "../context/PasswordContext";
 
 export default function GeneratePasswordButton() {
-  const { length, options, generateRandomPassword } = useContext(PasswordContext);
-  const [buttonColor, setButtonColor] = useState('#A4FFAF');
+  const { length, options, generateRandomPassword } =
+    useContext(PasswordContext);
+  const [buttonColor, setButtonColor] = useState("#A4FFAF");
+  const [buttonTextColor, setButtonTextColor] = useState("text-darkgrey");
+  const [buttonBorderColor, setButtonBorderColor] = useState("");
+  const [svgColor, setSvgColor] = useState("#18171F");
 
   const genRandomPass = () => {
     generateRandomPassword(length, options);
-  }
+  };
 
-const buttonColorTransition = () => {
-  setButtonColor('#817D92');
-  setTimeout(() => {
-    setButtonColor('#A4FFAF');
-  }, 150);
-}
+  const buttonColorTransition = () => {
+    setButtonColor("#24232C");
+    setButtonTextColor("text-green");
+    setButtonBorderColor("border border-solid border-green");
+    setSvgColor("#A4FFAF");
+    setTimeout(() => {
+      setButtonColor("#A4FFAF");
+      setButtonTextColor("text-darkgrey");
+      setButtonBorderColor("");
+      setSvgColor("#18171F");
+    }, 150);
+  };
 
-const handleClick = () => {
-  buttonColorTransition();
-  genRandomPass();
-}
+  const handleClick = () => {
+    buttonColorTransition();
+    genRandomPass();
+  };
 
   return (
     <div className="">
-      <button className="text-darkgrey text-m font-normal bg-green w-full h-14 flex items-center justify-center "
-      style={{ backgroundColor: buttonColor }}
-      onClick={() =>{handleClick()}}
+      <button
+        className={`text-m font-normal w-full h-14 flex items-center justify-center ${buttonTextColor} ${buttonBorderColor}`}
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => {
+          handleClick();
+        }}
       >
         GENERATE
-        <div className="ml-5"><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg></div>
+        <div className="ml-5">
+          <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"
+              fill={svgColor}
+            />
+          </svg>
+        </div>
       </button>
     </div>
   );
